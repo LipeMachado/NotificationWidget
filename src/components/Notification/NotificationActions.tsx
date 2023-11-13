cde2efa6d5a670388cc0396bcba0b1782f3c3910
LipeMachado <silvalipe765@gmail.com>
2023-11-13 12:04:44 -0300

    Add TwMerge and Pattern

---
+++ src/components/Notification/NotificationActions.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface NotificationProps {
+    children: ReactNode
+}
+
+export function NotificationActions({ children }: NotificationProps) {
+    return (
+        <>
+            <div className="flex gap-2 self-center">
+                {children}
+            </div>
+        </>
+    )
+}
